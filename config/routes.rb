61d70f206268d378ae6253f4e5cc34ed72e2a579
EC2 Default User <ec2-user@ip-172-31-71-75.ec2.internal>
2024-11-05 10:36:09 +0000

    実装フェーズ1完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
-  
   resources :posts
-  
   resources :post_images
   root "homes#top"
-  #get "/home/about" => "homes#about"
   get 'tagsearches/search', to: 'tagsearches#search'
 end

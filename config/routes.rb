7d2dfb5e50556d52d181562cd580108ced675ea5
EC2 Default User <ec2-user@ip-172-31-71-75.ec2.internal>
2024-11-05 10:29:04 +0000

    Update routes configuration:wq

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
+  
   resources :posts
+  
   resources :post_images
   root "homes#top"
   #get "/home/about" => "homes#about"

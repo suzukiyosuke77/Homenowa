0c862d6b59e60a3ee3e1e8d7ae66489e9617c269
EC2 Default User <ec2-user@ip-172-31-42-25.eu-north-1.compute.internal>
2025-01-23 11:09:13 +0000

    deploy settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,8 +29,6 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.assets.digest = true
-  config.assets.css_compressor = :sass
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
--- config/puma.rb
+++ config/puma.rb
@@ -1,28 +1,32 @@
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS", 5).to_i
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }.to_i
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
-port ENV.fetch("PORT", 3000)
-environment ENV.fetch("RAILS_ENV", "development")
+port ENV.fetch("PORT") { 3000 }
+environment ENV.fetch("RAILS_ENV") { "development" }
 
-pidfile ENV.fetch("PIDFILE", "tmp/pids/server.pid")
+pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
-workers ENV.fetch("WEB_CONCURRENCY", 2).to_i
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 preload_app!
 
 plugin :tmp_restart
 
-rails_root = File.expand_path("..", __dir__)
-bind "unix://#{rails_root}/tmp/sockets/puma.sock"
+rails_root = Dir.pwd
 
-if ENV.fetch("RAILS_ENV", "development") == "production"
-  pidfile File.join(rails_root, "tmp", "pids", "puma.pid")
-  state_path File.join(rails_root, "tmp", "pids", "puma.state")
+bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+rails_root = Dir.pwd
+# 本番環境のみデーモン起動
+if Rails.env.production?
+  pidfile File.join(rails_root, 'tmp', 'pids', 'puma.pid')
+  state_path File.join(rails_root, 'tmp', 'pids', 'puma.state')
   stdout_redirect(
-    File.join(rails_root, "log", "puma.log"),
-    File.join(rails_root, "log", "puma-error.log"),
+    File.join(rails_root, 'log', 'puma.log'),
+    File.join(rails_root, 'log', 'puma-error.log'),
     true
   )
+  # デーモン
+  daemonize
 end

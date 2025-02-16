d9e609e4ba9d452e0226dc2070a62722a73f6136
EC2 Default User <ec2-user@ip-172-31-42-25.eu-north-1.compute.internal>
2025-02-16 13:20:23 +0000

    通報機能作成

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,19 @@
 class ReportsController < ApplicationController
+
+  
   def create
+    @report = @post.reports.new(report_params)
+    @report.user = current_user
+
+    if @report.save
+      flash[:notice] = "通報を受け付けました"
+    else
+      flash[:alert] = "通報に失敗しました..."
+    end
+    redirect_to @post
+  end
+
+
   end
 
   def index
